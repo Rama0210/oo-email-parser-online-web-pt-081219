e0cb7fec2db69a409bdfce35fe11424b5c4a7c85
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-21 02:16:02 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,21 +1,11 @@
-class EmailParser
-	attr_accessor :emails
-	
-	def initialize(emails)
-		@emails= emails
-	end
-	
-	def parse
-		@emails = @emails.gsub(/[\s,]/ ," ").split
-		# @email is an array now
-		@emails.uniq
-	end
-	
-end
-
- #emails = "john@doe.com, person@somewhere.org"
-  #parser = EmailParser.new(emails)
-  #parser.parse => ["@john@doe.com", "person@somewhere.org"]
- 
-  
- 
+ attr_accessor :emails
+ def initialize(emails)
+   @emails = emails
+ end
+ def parse
+   emails.split.collect do |link|
+     link.split(',')
+   end
+   .flatten.uniq
+ end
+ end
